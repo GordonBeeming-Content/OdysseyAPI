4f99863298aebb72afaebfd8f36bed84c18a1425
Gordon Beeming <gpg@gordonbeeming.com>
2023-02-12 17:03:58 +0200

    ✨ Contacts controller wired up

--- src/OdysseyAPI/Controllers/ContactsController.cs
+++ src/OdysseyAPI/Controllers/ContactsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OdysseyAPI.Data;
 using OdysseyAPI.Models;
 
 namespace OdysseyAPI.Controllers;
@@ -7,27 +8,96 @@ namespace OdysseyAPI.Controllers;
 [Route("[controller]")]
 public sealed class ContactsController : ControllerBase
 {
-  private static readonly string[] _summaries = new[]
-  {
-      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-  };
-
   private readonly ILogger<ContactsController> _logger;
+  private readonly PhonebookDbContext _phonebookDbContext;
 
-  public ContactsController(ILogger<ContactsController> logger)
+  public ContactsController(ILogger<ContactsController> logger, PhonebookDbContext phonebookDbContext)
   {
     _logger = logger;
+    _phonebookDbContext = phonebookDbContext;
   }
 
-  [HttpGet(Name = "GetWeatherForecast")]
-  public IEnumerable<WeatherForecast> Get()
+  [HttpGet]
+  [ProducesResponseType(typeof(List<ContactModel>), StatusCodes.Status200OK)]
+  public async Task<ActionResult> Get()
   {
-    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    _logger.LogInformation("Getting contacts");
+    var result = await _phonebookDbContext.Contacts
+      .Select(o => new ContactModel
+      {
+        Id = o.Id,
+        Name = o.Name,
+        Number = o.Number
+
+      })
+      .ToListAsync();
+    return Ok(result);
+  }
+
+  [HttpGet("{id}")]
+  [ProducesResponseType(typeof(ContactModel), StatusCodes.Status200OK)]
+  [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+  public async Task<ActionResult> GetById(int id)
+  {
+    _logger.LogInformation("Getting contact {Contact Id}", id);
+    var result = await _phonebookDbContext.Contacts
+      .Select(o => new ContactModel
+      {
+        Id = o.Id,
+        Name = o.Name,
+        Number = o.Number
+
+      })
+      .FirstOrDefaultAsync(o => o.Id == id);
+    if (result == null)
     {
-      Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-      TemperatureC = Random.Shared.Next(-20, 55),
-      Summary = _summaries[Random.Shared.Next(_summaries.Length)]
-    })
-    .ToArray();
+      return NotFound();
+    }
+    return Ok(result);
+  }
+
+  [HttpPut]
+  [ProducesResponseType(typeof(ContactModel), StatusCodes.Status201Created)]
+  [Consumes("application/json")]
+  public async Task<ActionResult> Create(ContactModel model)
+  {
+    _logger.LogInformation("Creating new contact");
+    var contact = new Contact
+    {
+      Name = model.Name,
+      Number = model.Number
+    };
+    _phonebookDbContext.Contacts.Add(contact);
+    await _phonebookDbContext.SaveChangesAsync();
+
+    var dbContact = await _phonebookDbContext.Contacts
+      .FirstOrDefaultAsync(o => o.Id == contact.Id);
+    model = new ContactModel
+    {
+      Id = dbContact!.Id,
+      Name = dbContact.Name,
+      Number = dbContact.Number
+    };
+    var getUri = Url.Action(nameof(GetById), new { id = model.Id, });
+    return Created(getUri!, model);
+  }
+
+  [HttpPost]
+  [ProducesResponseType(typeof(ContactModel), StatusCodes.Status200OK)]
+  [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+  [Consumes("application/json")]
+  public async Task<ActionResult> Update(ContactModel model)
+  {
+    _logger.LogInformation("Updating contact {Contact Id}", model.Id);
+    var contact = await _phonebookDbContext.Contacts
+      .FirstOrDefaultAsync(o => o.Id == model.Id);
+    if (contact == null)
+    {
+      return NotFound();
+    }
+    contact.Name = model.Name;
+    contact.Number = model.Number;
+    await _phonebookDbContext.SaveChangesAsync();
+    return Ok(model);
   }
 }
---
+++ src/OdysseyAPI/Models/ContactModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OdysseyAPI.Models;
+
+#pragma warning disable CS8618
+public sealed class ContactModel
+{
+  public int Id { get; set; }
+
+  public string Name { get; set; }
+
+  public string Number { get; set; }
+}
